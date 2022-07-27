96b5c72a03abca6106fcbd5cc812fd66f8600747
SigireddyBalasai <101649913+SigireddyBalasai@users.noreply.github.com>
2022-07-27 08:25:30 +0530

    Update dba.py

--- infamous_imps/server/dba.py
+++ infamous_imps/server/dba.py
@@ -1,9 +1,8 @@
-import os
 from os.path import exists
 import sqlite3
 
 def start():
-        """ this programmm will check if database exists in server if not create it"""
+        """ This programmm will check if database exists in server if not create it"""
         if(exists("data.db")):
             pass
         else:
@@ -18,22 +17,28 @@ def start():
             con.commit()
             con.close()
 start()
+
+
 def add_message(server,timestamp,user,text):
-    """ adds new message to the server"""
+    """ Adds new message to the server"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     cur.execute("insert into chat values (?,?,?)", (user,timestamp,text))
     con.commit()
     con.close()
+        
+        
 def add_user(name,hashed):
-    "adds new player to the database"""
+    "Adds new player to the database"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     cur.execute("insert into auth values (?,?)", (name,hashed))
     con.commit()
     con.close()
+        
+        
 def delete_user(name,hashed):
-    """deletes the user from the database"""
+    """Deletes the user from the database"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     print(name)
@@ -41,11 +46,14 @@ def delete_user(name,hashed):
     cur.execute('DELETE from auth where user =?',name)
     con.commit()
     con.close()
-def see_message(self,server):
-    """see all message from the server"""
+        
+def see_message(server):
+    """See all message from the server"""
+    con = sqlite3.connect("data.db") 
+    cur = con.cursor()
     return cur.execute('SELECT * FROM server ORDER BY timestamp')
 def check_user(name,hashed):
-    """check if given credentials are correct"""
+    """Check if given credentials are correct"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     name = (name,)
@@ -57,8 +65,10 @@ def check_user(name,hashed):
         return True
     else:
         return False
+
+
 def update_user_name(name,hashed,new_name):
-    """update the given user name"""
+    """Update the given user name"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     ok = check_user(name, hashed)
@@ -68,8 +78,10 @@ def update_user_name(name,hashed,new_name):
      cur.execute('UPDATE auth set user = ? where password =?',name)
      con.commit()
     con.close()
+
+
 def update_password(name,hashed,password):
-    """update the given password"""
+    """Update the given password"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     print(name,hashed)
