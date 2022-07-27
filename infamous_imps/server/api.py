67674b4c1120d772914cf06194bffddebad35017
SigireddyBalasai <101649913+SigireddyBalasai@users.noreply.github.com>
2022-07-27 08:29:17 +0530

    Update api.py

--- infamous_imps/server/api.py
+++ infamous_imps/server/api.py
@@ -1,22 +1,16 @@
 from fastapi import FastAPI
-
-app = FastAPI()
-import jinja2
 import time
 from dba import add_message,delete_user,add_user,update_user_name
 
-
+app = FastAPI()
 @app.post("/chat/{server}")
 async def chat(server:str,user:str,message:str):
     """Post new message to server """
-
     timestamp = time.time()
-    user = data["user"]
-    message = data["message"]
     add_message(server,timestamp,user,message)
    
 
-@pp.post("/api/cv1/player/delete/{name}&hash={hashed}")
+@app.post("/api/cv1/player/delete/{name}&hash={hashed}")
 async def del_user(name:str,hashed:str):
     """ Delete user from the server """
     delete_user(name, hashed)
