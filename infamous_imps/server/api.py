8e9ec80d974f9c0d5355ed2f6a9353d2c946c652
SigireddyBalasai <101649913+SigireddyBalasai@users.noreply.github.com>
2022-07-27 08:20:37 +0530

    Update api.py

--- infamous_imps/server/api.py
+++ infamous_imps/server/api.py
@@ -1,50 +1,33 @@
-from aiohttp import web
-import aiohttp_jinja2
+from fastapi import FastAPI
+
+app = FastAPI()
 import jinja2
 import time
 from dba import add_message,delete_user,add_user,update_user_name
 
 
-app = web.Application()
-aiohttp_jinja2.setup(app,loader=jinja2.FileSystemLoader(["templates","server/templates"]))
-routes = web.RouteTableDef()
-
-
-@routes.post("/chat/{server}")
-async def chat(request):
+@app.post("/chat/{server}")
+async def chat(server:str,user:str,message:str):
     """Post new message to server """
-    data = request.post()
-    server = request.match_info['server']
+
     timestamp = time.time()
     user = data["user"]
     message = data["message"]
     add_message(server,timestamp,user,message)
    
 
-@routes.post("/api/cv1/player/delete/{name}&hash={hashed}")
-async def del_user(request):
+@pp.post("/api/cv1/player/delete/{name}&hash={hashed}")
+async def del_user(name:str,hashed:str):
     """ Delete user from the server """
-    print(request)
-    name = request.match_info['name']
-    hashed = request.match_info['hashed']
     delete_user(name, hashed)
     
     
-@routes.post("/api/v1/player/create/{name}?password={hash}")
-async def new_user(request):
+@app.post("/api/v1/player/create/{name}?password={hashed}")
+async def new_user(name:str,hashed:str):
     """ Adds user to the server """
-    print(request)
-    name = request.match_info['name']
-    hashed = request.match_info['hash']
     add_user(name, hashed)
-@routes.post("/api/cv1/player/update/{name}?name={new_name}?password={hashed}")
-async def update_user(request):
+@app.post("/api/cv1/player/update/{name}?name={new_name}?password={hashed}")
+async def update_user(name,new_name,hashed):
     """Update user credentials"""
-    print(request)
-    name = request.match_info['name']
-    hashed = request.match_info['hashed']
-    new_name = request.match_info['new_name']
     update_user_name(name, hashed, new_name)
 
-
-app.add_routes(routes)
