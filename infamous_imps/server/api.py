29ed9baa2b6930c8a8ad82e03889fb091669e7a8
SigireddyBalasai <101649913+SigireddyBalasai@users.noreply.github.com>
2022-07-27 21:39:43 +0530

    done

--- infamous_imps/server/api.py
+++ infamous_imps/server/api.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 import time
 from dba import add_message,delete_user,add_user,update_user_name
 
+
 app = FastAPI()
 @app.post("/chat/{server}")
 async def chat(server:str,user:str,message:str):
     """Post new message to server """
     timestamp = time.time()
     add_message(server,timestamp,user,message)
+    print(server,user,message)
    
 
 @app.post("/api/cv1/player/delete/{name}&hash={hashed}")
@@ -24,4 +26,3 @@ async def new_user(name:str,hashed:str):
 async def update_user(name,new_name,hashed):
     """Update user credentials"""
     update_user_name(name, hashed, new_name)
-
--- infamous_imps/server/dba.py
+++ infamous_imps/server/dba.py
@@ -2,8 +2,9 @@ from os.path import exists
 import sqlite3
 
 def start():
-        """ This programmm will check if database exists in server if not create it"""
-        if(exists("data.db")):
+        # noinspection SpellCheckingInspection
+        """ This programm will check if database exists in server if not create it"""
+        if exists("data.db"):
             pass
         else:
             f = open("data.db","w")
@@ -11,7 +12,7 @@ def start():
             con = sqlite3.connect("data.db")
             cur = con.cursor()
             cur.execute('''CREATE TABLE chat
-               (user text,timestamp real,message text)''')
+               (server text,user text,timestamp real,message text)''')
             cur.execute('''CREATE TABLE auth
                (user text, password text)''')
             con.commit()
@@ -23,7 +24,7 @@ def add_message(server,timestamp,user,text):
     """ Adds new message to the server"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
-    cur.execute("insert into chat values (?,?,?)", (user,timestamp,text))
+    cur.execute("insert into chat values (?,?,?,?)", (server,user,timestamp,text))
     con.commit()
     con.close()
         
@@ -36,14 +37,28 @@ def add_user(name,hashed):
     con.commit()
     con.close()
         
-        
+def check_user(name,hashed):
+    """Check if given credentials are correct"""
+    con = sqlite3.connect("data.db")
+    cur = con.cursor()
+    name = (name,)
+    c = cur.execute('SELECT * FROM auth where user = ?',name)
+    ok = c.fetchall()[0][1]
+    con.commit()
+    con.close()
+    if ok == hashed:
+        return True
+    else:
+        return False
+
 def delete_user(name,hashed):
     """Deletes the user from the database"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     print(name)
-    name = (name,)
-    cur.execute('DELETE from auth where user =?',name)
+    if check_user(name,hashed):
+        name = (name,)
+        cur.execute('DELETE from auth where user =?',name)
     con.commit()
     con.close()
         
@@ -51,30 +66,17 @@ def see_message(server):
     """See all message from the server"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
-    return cur.execute('SELECT * FROM server ORDER BY timestamp')
-def check_user(name,hashed):
-    """Check if given credentials are correct"""
-    con = sqlite3.connect("data.db") 
-    cur = con.cursor()
-    name = (name,)
-    c = cur.execute('SELECT * FROM auth where user = ?',name)
-    ok = c.fetchall()[0][1]
-    con.commit()
-    con.close()
-    if(ok == hashed):
-        return True
-    else:
-        return False
+    return cur.execute('SELECT * FROM chat ORDER BY timestamp where server = ?',(server,))
 
 
 def update_user_name(name,hashed,new_name):
-    """Update the given user name"""
+    """Update the given username"""
     con = sqlite3.connect("data.db") 
     cur = con.cursor()
     ok = check_user(name, hashed)
     name = (new_name,hashed)
     print(ok)
-    if(ok):
+    if ok:
      cur.execute('UPDATE auth set user = ? where password =?',name)
      con.commit()
     con.close()
@@ -88,7 +90,7 @@ def update_password(name,hashed,password):
     ok = check_user(name,hashed)
     name = (password,name)
     print(ok)
-    if(ok):
+    if ok:
      cur.execute('UPDATE auth set password = ? where user =?',name)
      con.commit()
     con.close()
